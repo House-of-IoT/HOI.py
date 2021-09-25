1793f7c192028c3b308c539d7b271ac54f62853c
RonaldColyar <phantexcreations@gmail.com>
2021-09-24 19:56:16 -0500

    init

---
+++ Client/device_config.py
@@ -0,0 +1,8 @@
+class Config:
+    def __init__(self,port,host,password,tasks,name,type_data):
+        self.port = port
+        self.host = host
+        self.password = password
+        self.tasks = tasks
+        self.name = name
+        self.type = type_data

3af605a9831228e438aeb4b911636fa69fef2459
RonaldColyar <phantexcreations@gmail.com>
2021-09-25 12:24:13 -0500

    new

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-phantexcreations",
-    version="16.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    name="hoi-client-phantexcreations",
+    version="1.0.0",
+    author="ronaldcolyar",
+    install_requires=["websockets"],
+    description="HOI client library",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/House-of-IoT/HOI.py",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/House-of-IoT/HOI.py/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",

87d320afa337f3b13067d3e1f338868cac266633
RonaldColyar <PhantexCreations@gmail.com>
2021-10-17 15:51:08 -0500

    init for tests

---
+++ src/hoi_client/tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+class Tests(unittest.IsolatedAsyncioTestCase):
+
+    async def test():
+        pass
+
+    async def authentication(websocket):
+        pass
+
+    async def requirements_check():
+        pass
+    
+    async def task_execution():
+        pass
+
+    async def name_and_type():
+        pass
